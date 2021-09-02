dd72b169216915ff82d7d92c33b1958a7910f433
VInicius Ferreira <contato.vinicius-af@outlook.com>
2021-09-01 22:17:43 -0300

    teste importação bootstrap

--- app/views/home.php
+++ app/views/home.php
@@ -4,9 +4,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="./app/assets/libraries/bootstrap.css" rel="stylesheet">
+    <script src="./app/assets/libraries/bootstrap.js"></script>
     <title>Document</title>
 </head>
 <body>
-    <h1>home</h1>
+    <h1>home</h1> <button class="btn btn-warning">Teste bootstrap</button>
 </body>
 </html>
