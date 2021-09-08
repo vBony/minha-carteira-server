ee6d87e44decce7fd28b64dcf99c757f35a77af3
VInicius Ferreira <contato.vinicius-af@outlook.com>
2021-09-07 21:09:53 -0300

    (#2) - Cadastro básico dos usuários

--- app/controllers/userController.php
+++ app/controllers/userController.php
@@ -18,7 +18,17 @@ class userController extends controllerHelper{
     public function register(){
         $Usuario = new Usuario();
         $data = $_POST;
+
+        if(!$Usuario->validate($data)){
+            $this->sendJson(array("errors" => $Usuario->errors));
+        }else{
+            if(!empty($Usuario->buscarPorEmail($data['usu_email']))){
+                $this->sendJson(array("errors" => ['usu_email' => "Já existe uma conta cadastrada com esse e-mail"]));
+            }else{
+                $Usuario->cadastrar($data);
+                $this->sendJson(array('messages' => 'success'));
+            }
+        }
         
-        $this->sendJson($data);
     }
 }
--- config.php
+++ config.php
@@ -9,9 +9,9 @@ $dotenv->loadEnv(__DIR__.'/.env');
 
 global $db;
 try{
-    // $db = new PDO("mysql:dbname=".$_ENV['DB_NAME'].";host=".$_ENV['HOST'], $_ENV['DB_USER'], $_ENV['DB_PASS']);
+    $db = new PDO("mysql:dbname=".$_ENV['DB_NAME'].";host=".$_ENV['HOST'], $_ENV['DB_USER'], $_ENV['DB_PASS']);
 }catch(PDOException $e){
-    // echo "Error: ".$e->getMessage();
-    // exit;
+    echo "Error: ".$e->getMessage();
+    exit;
 }
 ?>
