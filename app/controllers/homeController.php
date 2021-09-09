962049bbc44a634967044fbd8b6ffed89ecb3489
VInicius Ferreira <contato.vinicius-af@outlook.com>
2021-09-08 22:43:00 -0300

    cards dashboard

--- app/controllers/homeController.php
+++ app/controllers/homeController.php
@@ -9,6 +9,7 @@ class homeController extends controllerHelper{
         $data['js'] = 'home.js';
         $data['title'] = "Minha carteira";
         $data['user_data'] = $_SESSION['user_data'];
+        $data['base_url'] = $_ENV['BASE_URL'];
         
         $this->loadTemplate('home', $data);
     }
--- app/views/home.php
+++ app/views/home.php
@@ -1,12 +1,41 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>home</h1>
-</body>
-</html>
+<div class="row">
+    <div class="col-lg-3 col-md-6 col-sm-12 mb-md-3 mb-sm-3 mb-3">
+        <div class="card border border-primary">
+            <div class="card-body">
+                <h5 class="card-title">Saldo Atual</h5>
+                <h6 class="card-subtitle mb-2">R$ 1.625,00</h6>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-lg-3 col-md-6 col-sm-12 mb-md-3 mb-sm-3 mb-3">
+        <div class="card border border-success">
+            <div class="card-body">
+                <h5 class="card-title">Receitas</h5>
+                <h6 class="card-subtitle mb-2">R$ 2.125,00</h6>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-lg-3 col-md-6 col-sm-12 mb-sm-3 mb-3">
+        <div class="card border border-danger">
+            <div class="card-body">
+                <h5 class="card-title">Despesas</h5>
+                <h6 class="card-subtitle mb-2">R$ 500,00</h6>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-lg-3 col-md-6 col-sm-12 mb-sm-3 mb-3">
+        <div class="card border border-info">
+            <div class="card-body">
+                <h5 class="card-title">Balanço mensal</h5>
+                <h6 class="card-subtitle mb-2">R$ 1.625,00</h6>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="row">
+    
+</div>
