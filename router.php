6fe3fdc74e5157065edc7ab2f541f16d1104231a
VInicius Ferreira <contato.vinicius-af@outlook.com>
2021-09-21 23:57:19 -0300

    retirando bug de conflito

--- router.php
+++ router.php
@@ -4,6 +4,5 @@ $routes = array();
 $routes['/criar-conta'] = '/user/criarConta';
 $routes['/api/criar-conta'] = '/user/register';
 $routes['/login'] = '/user/login';
-<<<<<<< Updated upstream
 $routes['/api/login'] = '/user/loginApi';
 $routes['/dashboard/inserir-receita'] = '/dashboard/inserirReceita';
